24838c32e5539a9f016a3ce22f854b56cd7ebcb9
gabylima <mattrocha57@gmail.com>
2021-02-11 08:29:12 -0300

    adcionando arquivospara deploy

---
+++ aprendizado_maquina_ufsc/projeto_final/data_app/setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"mattrocha57@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
